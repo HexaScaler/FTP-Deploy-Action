c9f98a8570a62c291c5d87c48d3ed022a4379881
Ahire Chetan <ahirechetan24@hotmail.com>
2023-02-11 09:28:24 +0530

    changes

--- dist/index.js
+++ dist/index.js
@@ -3276,8 +3276,8 @@ function deploy(args, logger, timings) {
         logger.all(`🚀 Thanks for using ftp-deploy. Let's deploy some stuff!   `);
         logger.all(`----------------------------------------------------------------`);
         logger.all(`If you found this project helpful, please support it`);
-        logger.all(`by giving it a ⭐ on Github --> https://github.com/SamKirkland/FTP-Deploy-Action`);
-        logger.all(`or add a badge 🏷️ to your projects readme --> https://github.com/SamKirkland/FTP-Deploy-Action#badge`);
+        logger.all(`by giving it a ⭐ on Github --> https://github.com/HexaScaler/FTP-Deploy-Action`);
+        logger.all(`or add a badge 🏷️ to your projects readme --> https://github.com/HexaScaler/FTP-Deploy-Action#badge`);
         logger.verbose(`Using the following excludes filters: ${JSON.stringify(args.exclude)}`);
         timings.start("hash");
         const localFiles = yield (0, localFiles_1.getLocalFiles)(args);
@@ -3708,7 +3708,7 @@ exports.FTPSyncProvider = FTPSyncProvider;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.ErrorCode = exports.syncFileDescription = exports.currentSyncFileVersion = void 0;
 exports.currentSyncFileVersion = "1.0.0";
-exports.syncFileDescription = "DO NOT DELETE THIS FILE. This file is used to keep track of which files have been synced in the most recent deployment. If you delete this file a resync will need to be done (which can take a while) - read more: https://github.com/SamKirkland/FTP-Deploy-Action";
+exports.syncFileDescription = "DO NOT DELETE THIS FILE. This file is used to keep track of which files have been synced in the most recent deployment. If you delete this file a resync will need to be done (which can take a while) - read more: https://github.com/HexaScaler/FTP-Deploy-Action";
 var ErrorCode;
 (function (ErrorCode) {
     // The requested action is being initiated, expect another reply before proceeding with a new command.
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from "@actions/core";
-import { deploy } from "@samkirkland/ftp-deploy";
-import { IFtpDeployArguments } from "@samkirkland/ftp-deploy/dist/types";
+import { deploy } from "@HexaScaler/ftp-deploy";
+import { IFtpDeployArguments } from "@HexaScaler/ftp-deploy/dist/types";
 import { optionalInt, optionalProtocol, optionalString, optionalBoolean, optionalStringArray, optionalLogLevel, optionalSecurity } from "./parse";
 
 async function runDeployment() {
